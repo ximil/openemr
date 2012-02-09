a245e4991d2d3865b946afe087e9e03df11aa9bf
Jacob T.Paul <jacob@zhservices.com>
2012-02-09 13:04:43 +0530

    Fix CRLF

---
+++ sql/4_1_0-to-4_1_1_upgrade.sql
@@ -0,0 +1,169 @@
+--
+--  Comment Meta Language Constructs:
+--
+--  #IfNotTable
+--    argument: table_name
+--    behavior: if the table_name does not exist,  the block will be executed
+
+--  #IfTable
+--    argument: table_name
+--    behavior: if the table_name does exist, the block will be executed
+
+--  #IfMissingColumn
+--    arguments: table_name colname
+--    behavior:  if the colname in the table_name table does not exist,  the block will be executed
+
+--  #IfNotColumnType
+--    arguments: table_name colname value
+--    behavior:  If the table table_name does not have a column colname with a data type equal to value, then the block will be executed
+
+--  #IfNotRow
+--    arguments: table_name colname value
+--    behavior:  If the table table_name does not have a row where colname = value, the block will be executed.
+
+--  #IfNotRow2D
+--    arguments: table_name colname value colname2 value2
+--    behavior:  If the table table_name does not have a row where colname = value AND colname2 = value2, the block will be executed.
+
+--  #IfNotRow3D
+--    arguments: table_name colname value colname2 value2 colname3 value3
+--    behavior:  If the table table_name does not have a row where colname = value AND colname2 = value2 AND colname3 = value3, the block will be executed.
+
+--  #IfNotRow4D
+--    arguments: table_name colname value colname2 value2 colname3 value3 colname4 value4
+--    behavior:  If the table table_name does not have a row where colname = value AND colname2 = value2 AND colname3 = value3 AND colname4 = value4, the block will be executed.
+
+--  #IfNotRow2Dx2
+--    desc:      This is a very specialized function to allow adding items to the list_options table to avoid both redundant option_id and title in each element.
+--    arguments: table_name colname value colname2 value2 colname3 value3
+--    behavior:  The block will be executed if both statements below are true:
+--               1) The table table_name does not have a row where colname = value AND colname2 = value2.
+--               2) The table table_name does not have a row where colname = value AND colname3 = value3.
+
+--  #IfNotIndex
+--    desc:      This function will allow adding of indexes/keys.
+--    arguments: table_name colname
+--    behavior:  If the index does not exist, it will be created
+
+--  #EndIf
+--    all blocks are terminated with and #EndIf statement.
+
+
+#IfNotIndex lists type
+CREATE INDEX `type` ON `lists` (`type`);
+#EndIf
+
+#IfNotIndex lists pid
+CREATE INDEX `pid` ON `lists` (`pid`);
+#EndIf
+
+#IfNotIndex form_vitals pid
+CREATE INDEX `pid` ON `form_vitals` (`pid`);
+#EndIf
+
+#IfNotIndex forms pid
+CREATE INDEX `pid` ON `forms` (`pid`);
+#EndIf
+
+#IfNotIndex form_encounter pid
+CREATE INDEX `pid` ON `form_encounter` (`pid`);
+#EndIf
+
+#IfNotIndex immunizations patient_id
+CREATE INDEX `patient_id` ON `immunizations` (`patient_id`);
+#EndIf
+
+#IfNotIndex procedure_order patient_id
+CREATE INDEX `patient_id` ON `procedure_order` (`patient_id`);
+#EndIf
+
+#IfNotIndex pnotes pid
+CREATE INDEX `pid` ON `pnotes` (`pid`);
+#EndIf
+
+#IfNotIndex transactions pid
+CREATE INDEX `pid` ON `transactions` (`pid`);
+#EndIf
+
+#IfNotIndex extended_log patient_id
+CREATE INDEX `patient_id` ON `extended_log` (`patient_id`);
+#EndIf
+
+#IfNotIndex prescriptions patient_id
+CREATE INDEX `patient_id` ON `prescriptions` (`patient_id`);
+#EndIf
+
+#IfMissingColumn version v_realpatch
+ALTER TABLE `version` ADD COLUMN `v_realpatch` int(11) NOT NULL DEFAULT 0;
+#EndIf
+
+#IfMissingColumn prescriptions drug_info_erx
+ALTER TABLE `prescriptions` ADD COLUMN `drug_info_erx` TEXT DEFAULT NULL;
+#EndIf
+
+#IfNotRow2D list_options list_id lists option_id nation_notes_replace_buttons
+INSERT INTO `list_options` (`list_id`, `option_id`, `title`, `seq`) VALUES ('lists','nation_notes_replace_buttons','Nation Notes Replace Buttons',1);
+INSERT INTO `list_options` (`list_id`, `option_id`, `title`, `seq`) VALUES ('nation_notes_replace_buttons','Yes','Yes',10);
+INSERT INTO `list_options` (`list_id`, `option_id`, `title`, `seq`) VALUES ('nation_notes_replace_buttons','No','No',20);
+INSERT INTO `list_options` (`list_id`, `option_id`, `title`, `seq`) VALUES ('nation_notes_replace_buttons','Normal','Normal',30);
+INSERT INTO `list_options` (`list_id`, `option_id`, `title`, `seq`) VALUES ('nation_notes_replace_buttons','Abnormal','Abnormal',40);
+#EndIf
+
+#IfMissingColumn insurance_data policy_type
+ALTER TABLE `insurance_data` ADD COLUMN `policy_type` varchar(25) NOT NULL default '';
+#EndIf
+
+#IfMissingColumn drugs max_level
+ALTER TABLE drugs ADD max_level float NOT NULL DEFAULT 0.0;
+ALTER TABLE drugs CHANGE reorder_point reorder_point float NOT NULL DEFAULT 0.0;
+#EndIf
+
+#IfNotTable product_warehouse
+CREATE TABLE `product_warehouse` (
+  `pw_drug_id`   int(11) NOT NULL,
+  `pw_warehouse` varchar(31) NOT NULL,
+  `pw_min_level` float       DEFAULT 0,
+  `pw_max_level` float       DEFAULT 0,
+  PRIMARY KEY  (`pw_drug_id`,`pw_warehouse`)
+) ENGINE=MyISAM;
+#EndIf
+
+# Increase size from 5 to 12 to support 4 modifiers with colon separation
+#IfNotColumnType billing modifier varchar(12)
+   ALTER TABLE `billing` MODIFY `modifier` varchar(12);
+   UPDATE `code_types` SET `ct_mod` = '12' where ct_key = 'CPT4' OR ct_key = 'HCPCS';
+#Endif
+
+#IfMissingColumn billing notecodes
+ALTER TABLE `billing` ADD `notecodes` varchar(25) NOT NULL default '';
+#EndIf
+
+
+
+#IfNotTable dated_reminders
+CREATE TABLE `dated_reminders` (
+            `dr_id` int(11) NOT NULL AUTO_INCREMENT,
+            `dr_from_ID` int(11) NOT NULL,
+            `dr_message_text` varchar(160) NOT NULL,
+            `dr_message_sent_date` datetime NOT NULL,
+            `dr_message_due_date` date NOT NULL,
+            `pid` int(11) NOT NULL,
+            `message_priority` tinyint(1) NOT NULL,
+            `message_processed` tinyint(1) NOT NULL DEFAULT '0',
+            `processed_date` timestamp NULL DEFAULT NULL,
+            `dr_processed_by` int(11) NOT NULL,
+            PRIMARY KEY (`dr_id`),
+            KEY `dr_from_ID` (`dr_from_ID`,`dr_message_due_date`)
+          ) ENGINE=MyISAM AUTO_INCREMENT=1;
+#EndIf
+
+#IfNotTable dated_reminders_link
+CREATE TABLE `dated_reminders_link` (
+            `dr_link_id` int(11) NOT NULL AUTO_INCREMENT,
+            `dr_id` int(11) NOT NULL,
+            `to_id` int(11) NOT NULL,
+            PRIMARY KEY (`dr_link_id`),
+            KEY `to_id` (`to_id`),
+            KEY `dr_id` (`dr_id`)
+          ) ENGINE=MyISAM AUTO_INCREMENT=1;
+#EndIf
