336bdf10839db8a1e8c066a0733ca4810a58fdd2
BASIL <basil@zhservices.com>
2013-03-11 16:54:55 +0530

    Modifications made to enable 'Pre Pay' Option in Self Payment Signed-off-by: BASIL <basil@zhservices.com>

--- interface/patient_file/front_payment.php
+++ interface/patient_file/front_payment.php
@@ -868,12 +868,16 @@ function make_it_hide()
 function make_visible_radio()
  {
   document.getElementById('tr_radio1').style.display="";
-  document.getElementById('tr_radio2').style.display="none";
+  document.getElementById('tr_radio2_text').style.display="none";
+  document.getElementById('tr_radio2_1').style.display="none";
+  document.getElementById('tr_radio2_2').style.display="none";
  }
 function make_hide_radio()
  {
   document.getElementById('tr_radio1').style.display="none";
-  document.getElementById('tr_radio2').style.display="";
+  document.getElementById('tr_radio2_text').style.display="";
+  document.getElementById('tr_radio2_1').style.display="";
+  document.getElementById('tr_radio2_2').style.display="";
  }
 function make_visible_row()
  {
@@ -978,9 +982,9 @@ function make_insurance()
  </tr>
  <tr id="tr_radio2"><!-- For radio self -->
   <td class='text' valign="top" >
-   <?php echo htmlspecialchars(xl('Payment against'), ENT_QUOTES); ?>:
+   <span id="tr_radio2_text" ><?php echo htmlspecialchars(xl('Payment against'), ENT_QUOTES); ?>:</span>
   </td>
-  <td class='text' colspan="2" ><input type="radio" name="radio_type_of_payment" id="radio_type_of_payment1" value="copay" checked="checked" onClick="make_visible_row();cursor_pointer();"/><?php echo htmlspecialchars(xl('Co Pay'), ENT_QUOTES); ?><input type="radio" name="radio_type_of_payment" id="radio_type_of_payment2" value="invoice_balance"  onClick="make_visible_row();"/><?php echo htmlspecialchars(xl('Invoice Balance'), ENT_QUOTES); ?><br/><input type="radio" name="radio_type_of_payment" id="radio_type_of_payment4" value="pre_payment" onClick="make_hide_row();"/><?php echo htmlspecialchars(xl('Pre Pay'), ENT_QUOTES); ?></td>
+  <td class='text' colspan="2" ><span id="tr_radio2_1" ><input type="radio" name="radio_type_of_payment" id="radio_type_of_payment1" value="copay" checked="checked" onClick="make_visible_row();cursor_pointer();"/><?php echo htmlspecialchars(xl('Co Pay'), ENT_QUOTES); ?></span><span id="tr_radio2_2" ><input type="radio" name="radio_type_of_payment" id="radio_type_of_payment2" value="invoice_balance"  onClick="make_visible_row();"/><?php echo htmlspecialchars(xl('Invoice Balance'), ENT_QUOTES); ?><br/></span><input type="radio" name="radio_type_of_payment" id="radio_type_of_payment4" value="pre_payment" onClick="make_hide_row();"/><?php echo htmlspecialchars(xl('Pre Pay'), ENT_QUOTES); ?></td>
  </tr>
 
  <tr height="15"><td colspan='3'></td></tr>
