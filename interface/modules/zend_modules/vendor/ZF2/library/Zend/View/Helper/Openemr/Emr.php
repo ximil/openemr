a619c4fb265a5acd0fad98562f02efb2707d51e8
Jacob T.Paul <jacob@zhservices.com>
2013-05-09 15:25:01 +0530

    Zend Library and modified autoload/globals.php for sql connection
    Signed-off-by: Jacob T.Paul <jacob@zhservices.com>

---
+++ interface/modules/zend_modules/vendor/ZF2/library/Zend/View/Helper/Openemr/Emr.php
@@ -0,0 +1,119 @@
+<?php
+namespace Zend\View\Helper\Openemr;
+use Zend\View\Helper\AbstractHelper;
+
+class Emr extends AbstractHelper
+{
+    /*
+    * function getList
+    * to get Particular list as an Array
+    * @param $list_id
+    * @value Id of a List
+    * @param $selected
+    * @value Id of a selected item
+    * @param $opt
+    * @value Option
+    */
+    public function getList($list_id,$selected='',$opt='')
+    {
+      $res = sqlStatement("SELECT * FROM list_options WHERE list_id=? ORDER BY seq, title",array($list_id));
+      $i = 0;
+      if ($opt == 'search') {
+          $rows[$i] = array (
+          'value' => 'all',
+          'label' => xlt('All'),
+          'selected' => TRUE,
+            );
+          $i++;
+      } elseif ($opt == '') {
+          $rows[$i] = array (
+            'value' => '',
+            'label' => xlt('Unassigned'),
+            'disabled' => FALSE
+          );
+          $i++;
+      }
+      while($row=sqlFetchArray($res)) {
+                $sel = ($row['option_id']==$selected) ? TRUE : FALSE;
+                $rows[$i] = array (
+                        'value' => htmlspecialchars($row['option_id'],ENT_QUOTES),
+                        'label' => xlt($row['title']),
+                        'selected' => $sel,
+                );
+                $i++;
+      }
+      return $rows;
+    }
+    /*
+    * function getProviders
+    * to get list of Providers as an Array
+    * @param void
+    */
+    public function getProviders()
+    {
+      $res = sqlStatement("SELECT id, fname, lname, specialty FROM users " .
+              "WHERE active = 1 AND ( info IS NULL OR info NOT LIKE '%Inactive%' ) " .
+              "AND authorized = 1 " .
+              "ORDER BY lname, fname"); 
+      $rows[0] = array (
+        'value' => '',
+        'label' => xlt('Unassigned'),
+        'selected' => TRUE,
+        'disabled' => FALSE
+      );
+      $i = 1;
+      
+      while($row=sqlFetchArray($res)) {
+        $rows[$i] = array (
+          'value' => $row['id'],
+          'label' => $row['fname']." ".$row['lname'],
+        );
+        $i++;
+      }
+      return $rows;
+    }
+    /*
+    * function getLabs
+    * to get list of labs
+    * @param $type
+    * @value 'y' - for type of Labs (Loacal or External)
+    */
+    public function getLabs($type='')
+    {
+      $res = sqlStatement("SELECT ppid,name,remote_host,login,password FROM procedure_providers ORDER BY name, ppid"); 
+      $i = 0;
+      
+      while($row=sqlFetchArray($res)) {
+          $value = '';
+          if ($type == 'y') {
+        if ($row['remote_host'] != '' && $row['login'] != '' && $row['password'] != '') {
+          $value = $row['ppid'] . '|' . 1; // 0 - Local Lab and 1 - External Lab
+        } else {
+          $value = $row['ppid'] . '|' . 0;
+        }
+          } else {
+        $value = $row['ppid'];
+          }
+          $rows[$i] = array (
+        'value' => $value,
+        'label' => $row['name'],
+          );
+          $i++;
+      }
+      return $rows;
+    }
+		
+    /*
+    * function getDropdownValAsText
+    * to get option title of a particular list
+    * @param $list_id
+    * @value ID of a list
+    * @param $option_id
+    * @value Value of option
+    */
+    public function getDropdownValAsText($list_id,$option_id)
+    {
+        $res = sqlQuery("SELECT title FROM list_options WHERE list_id = ? AND option_id = ?",array($list_id,$option_id)); 
+        return $res['title'];
+    }
+}
