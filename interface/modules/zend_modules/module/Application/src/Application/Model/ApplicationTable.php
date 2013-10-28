80cd1def9ac95743cd4fa347e3e7dc8882e55b2d
Ajil P M <ajilpm@zhservices.com>
2013-10-28 21:29:59 +0530

    Corrections.
    Signed-off-by: Ajil P M <ajilpm@zhservices.com>

--- interface/modules/zend_modules/module/Application/src/Application/Model/ApplicationTable.php
+++ interface/modules/zend_modules/module/Application/src/Application/Model/ApplicationTable.php
@@ -20,153 +20,26 @@ class ApplicationTable extends AbstractTableGateway
      */
     public function __construct()
     {
-        $adapter = \Zend\Db\TableGateway\Feature\GlobalAdapterFeature::getStaticAdapter();
-        $this->adapter = $adapter;
-        $this->resultSetPrototype = new ResultSet();
-        $this->resultSetPrototype->setArrayObjectPrototype(new Application());
-        $this->initialize();          
+      $adapter = \Zend\Db\TableGateway\Feature\GlobalAdapterFeature::getStaticAdapter();
+      $this->adapter = $adapter;
+      $this->resultSetPrototype = new ResultSet();
+      $this->resultSetPrototype->setArrayObjectPrototype(new Application());
+      $this->initialize();
     }
-    
-    /**
-     * 
-     * @return object
-     */
-    public function fetchAll()
-    {
-        $resultSet = $this->select();
-        return $resultSet;
-    }
-    
-    /**
-     * Select Query
-     * @param array $params
-     * @return array
-     */
-    public function selectTable($params)
-    {
-        $adapter = $this->adapter;
-        $sql = new Sql($adapter);
-        if ($params['fields'] != '*') {
-            $fields = $params['fields'];
-        }
-        $select = $sql->select($fields);
-        $select->from($params['tableName']);
-        
-        foreach ($params as $key => $value) { 
-            foreach ($value as $k => $v) {//echo '<pre>'; print_r($value); echo '</pre>';
-                if ($key == 'join') {
-                    $table = $v['table'];
-                    $on = $v['on'];
-                    $type = $v['type'];
-                    if ($v['fields']) {
-                            $select->join($table,$on,$v['fields'],$type);
-                    }
-                }
-            }
-            if ($key == 'where') 
-               $select->where($value);
-        }
-       // echo $select->__toString(); //exit;
-        $selectString = $sql->getSqlStringForSqlObject($select);
-        $this->log($selectString);
-        $results = $adapter->query($selectString, $adapter::QUERY_MODE_EXECUTE);
-        return $results;
-     }
-    
-    /**
-     * Insert Query
-     * @param array $params
-     * @return int Last insert id
-     */
-    public function insertTable($params)
-    {
-        $adapter = $this->adapter;
-        $sql = new Sql($adapter);
-        $insert = $sql->insert($params['tableName']);
-        $insert->values($params['fields']);
-        $selectString = $sql->getSqlStringForSqlObject($insert);
-        $this->log($selectString);
-        $results = $adapter->query($selectString, $adapter::QUERY_MODE_EXECUTE);
-        $lastId = $adapter->getDriver()->getLastGeneratedValue();
-        return $lastId;
-    }
-    
-    /**
-     * Update Query
-     * @param array $params
-     * @return array
-     */
-    public function updateTable($params)
-    { 
-        $adapter = $this->adapter;
-        $sql = new Sql($adapter);
-        $update = $sql->update($params['tableName']);
-        $update->set($params['fields']);
-        $update->where($params['where']);
-        $selectString = $sql->getSqlStringForSqlObject($update);
-        $this->log($selectString);
-        $results = $adapter->query($selectString, $adapter::QUERY_MODE_EXECUTE);
-    }
-    
-    /**
-     * Delete Query
-     * @param array $params
-     * @return array
-     */
-    public function deleteTable($params)
-    {
-        $adapter = $this->adapter;
-        $sql = new Sql($adapter);
-        $delete = $sql->delete();
-        $delete->from($params['tableName']);
-        $delete->where($params['where']);
-        $selectString = $sql->getSqlStringForSqlObject($delete);
-        $this->log($selectString);
-        $results = $adapter->query($selectString, $adapter::QUERY_MODE_EXECUTE);
-    }
-    
-    /**
-     * Count record
-     * @return int record count
-     */
-    public function rowCount($params)
-    {
-        $adapter = $this->adapter;
-        $sql = new Sql($adapter);
-        $select = $sql->select();
-        $select->from($params['tableName']);
-        $select->columns(array('total' => new \Zend\Db\Sql\Expression('COUNT(*)')));
-        
-        foreach ($params as $key => $value) { 
-            if ($key == 'where') 
-                $select->where($value);
-        }
-
-        $selectString = $sql->getSqlStringForSqlObject($select);
-        $this->log($selectString);
-        $results = $adapter->query($selectString, $adapter::QUERY_MODE_EXECUTE);
-        $resultSet = new ResultSet();
-        $resultSet->initialize($results);
-
-        $tmp = $resultSet->toArray();
-        $result = $tmp[0]['total'];
-        return $result;
-     }
      
-     public function sqlQuery($sql, $params = '')
-     {
-        $statement = $this->adapter->query($sql);
-        $return = $statement->execute($params);
-        $count = count($params);
-        $arr = array();
-        foreach($params as $val) {
-            array_push($arr, "'" . $val . "'");
-        }
-        $logSQL = preg_replace(array_fill(0, $count, "/\?/"), $arr, $sql, 1);
-        $this->log($logSQL);
-        
-        return $return;
-     }
+    public function sqlQuery($sql, $params = '')
+    {
+      $statement = $this->adapter->query($sql);
+      $return = $statement->execute($params);
+      $count = count($params);
+      $arr = array();
+      foreach($params as $val) {
+        array_push($arr, "'" . $val . "'");
+      }
+      $logSQL = preg_replace(array_fill(0, $count, "/\?/"), $arr, $sql, 1);
+      $this->log($logSQL);
+      return $return;
+    }
     
     /**
      * Log all DB Transactions
@@ -178,18 +51,14 @@ class ApplicationTable extends AbstractTableGateway
      */
     public function log($logSQL)
     {
-        $sql        = "INSERT INTO log SET date = ? , 
-                                user = ?, 
-                                groupname = ?, 
-                                comments = ?";
-        
-        $dt         = date('Y-m-d  H:i:s');
-        $user       = $_SESSION['authUser'];
-        $group      = $_SESSION['authGroup'];
-        $params     = array($dt, $user, $group, $logSQL);
-        $statement  = $this->adapter->query($sql);
-        $return     = $statement->execute($params);
-        return true;
+      $sql        = "INSERT INTO log SET date = ? ,user = ?, groupname = ?, comments = ?";
+      $dt         = date('Y-m-d  H:i:s');
+      $user       = $_SESSION['authUser'];
+      $group      = $_SESSION['authGroup'];
+      $params     = array($dt, $user, $group, $logSQL);
+      $statement  = $this->adapter->query($sql);
+      $return     = $statement->execute($params);
+      return true;
     }
     
 }
