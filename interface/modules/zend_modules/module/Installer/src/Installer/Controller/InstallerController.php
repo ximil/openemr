2c824928a61699d36a0ab43a394ac1aaf43f489b
vipin <vipink@zhservices.com>
2013-10-29 22:54:48 +0530

    Adding License texts

--- interface/modules/zend_modules/module/Installer/src/Installer/Controller/InstallerController.php
+++ interface/modules/zend_modules/module/Installer/src/Installer/Controller/InstallerController.php
@@ -1,30 +1,24 @@
 <?php
-// +-----------------------------------------------------------------------------+ 
-// Copyright (C) 2013 Z&H Consultancy Services Private Limited <sam@zhservices.com>
+// +-----------------------------------------------------------------------------+
+//OpenEMR - Open Source Electronic Medical Record
+//    Copyright (C) 2013 Z&H Consultancy Services Private Limited <sam@zhservices.com>
 //
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Affero General Public License as
+//    published by the Free Software Foundation, either version 3 of the
+//    License, or (at your option) any later version.
 //
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Affero General Public License for more details.
 //
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-//
-// A copy of the GNU General Public License is included along with this program:
-// openemr/interface/login/GnuGPL.html
-// For more information write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-// 
-// Author:   Jacob T.Paul <jacob@zhservices.com>
-//           Shalini Balakrishnan  <shalini@zhservices.com>
-//					 Remesh Babu S <remesh@zhservices.com>	
-//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 		Author:   Jacob T.Paul <jacob@zhservices.com>
+//							Vipin Kumar <vipink@zhservices.com>
 // +------------------------------------------------------------------------------+
+
 namespace Installer\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
