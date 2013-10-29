cfe0598c0f3c17d2c71dc0e6933a590ea5fdaa7a
vipin <vipink@zhservices.com>
2013-10-30 01:07:40 +0530

    Removed configuration and put sample one

--- interface/modules/zend_modules/module/Acl/src/Acl/Model/Configuration.php
+++ interface/modules/zend_modules/module/Acl/src/Acl/Model/Configuration.php
@@ -1,24 +1,4 @@
 <?php
-// +-----------------------------------------------------------------------------+
-//OpenEMR - Open Source Electronic Medical Record
-//    Copyright (C) 2013 Z&H Consultancy Services Private Limited <sam@zhservices.com>
-//
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU Affero General Public License as
-//    published by the Free Software Foundation, either version 3 of the
-//    License, or (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU Affero General Public License for more details.
-//
-//    You should have received a copy of the GNU Affero General Public License
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// 		Author:   Jacob T.Paul <jacob@zhservices.com>
-//							Vipin Kumar <vipink@zhservices.com>
-// +------------------------------------------------------------------------------+
-
 namespace Acl\Model;
 
 use Zend\InputFilter\Factory as InputFactory;
@@ -110,42 +90,64 @@ class Configuration extends Form implements InputFilterAwareInterface
     public function getHookConfig()
     {
 				//SHOULD SPECIFY THE CONTROLLER AND ITS ACTION IN THE PATH, INCLUDING INDEX ACTION
-				$hooks	=  array(
+				
+				/*
+				 //SAMPLE CONFIGURATION
+				 $hooks	=  array(
 												'0' => array(
-														'name' 	=> "procedure_order",
-														'title' 	=> "Procedure Order",
-														'path' 	=> "public/lab/index",
+														'name' 	=> "HookName1",
+														'title' 	=> "HookTitle1",
+														'path' 	=> "path/to/Hook1",
 												),
 												'1' => array(
-														'name' 	=> "procedure_result",
-														'title' 	=> "Lab Result",
-														'path' 	=> "public/lab/result/index",
+														'name' 	=> "HookName2",
+														'title' 	=> "HookTitle2",
+														'path' 	=> "path/to/Hook2",
 												),									
-										);	
+										);*/
+				
+				$hooks	=  array();	
 	
 				return $hooks;
     }
     public function getAclConfig()
     {
-				$acl = array(
+				/*
+				 //SAMPLE CONFIGURATION
+				 $acl = array(
+						array(
+							'section_id' 				=> 'SectionID1',
+							'section_name' 			=> 'SectionDisplayName1',
+							'parent_section' 		=> 'ParentSectionID1',
+							),
+						array(
+							'section_id' 				=> 'SectionID2',
+							'section_name' 			=> 'SectionDisplayName2',
+							'parent_section' 		=> 'ParentSectionID2',
+							),
 						);
+				*/
+				$acl = array();
 				return $acl;
 		}
     
     public function configSettings()
     {
-        $settings = array(
+        /*
+				 //SAMPLE CONFIGURATION
+				 $settings = array(
             array(
-                'display'   => 'HIE Path',
-                'field'     => 'hie_path',
-                'type'      => 'text',
+                'display'   => 'Display1',
+                'field'     => 'Filed1',
+                'type'      => 'FieldType1',
             ),
             array(
-                'display'   => 'Test',
-                'field' => 'user',
-                'type'  => 'select',
+                'display'   => 'Display2',
+                'field'     => 'Filed2',
+                'type'      => 'FieldType2',
             ),
-        );
+        );*/
+				$settings = array();
         return $settings;
     }
 		
