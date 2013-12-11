0e79b62822f672ae75677172ee6794bb4572445c
Remesh Babu S <remesh@zhservices.com>
2013-12-11 10:25:02 +0530

    Modifications for module installer in openemr.
    Signed-off-by: Remesh Babu S <remesh@zhservices.com>

--- interface/modules/zend_modules/public/index.php
+++ interface/modules/zend_modules/public/index.php
@@ -1,17 +1,46 @@
 <?php
+/* +-----------------------------------------------------------------------------+
+*    OpenEMR - Open Source Electronic Medical Record
+*    Copyright (C) 2013 Z&H Consultancy Services Private Limited <sam@zhservices.com>
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU Affero General Public License as
+*    published by the Free Software Foundation, either version 3 of the
+*    License, or (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU Affero General Public License for more details.
+*
+*    You should have received a copy of the GNU Affero General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*    @author  Jacob T.Paul <jacob@zhservices.com>
+*    @author  Shalini Balakrishnan <shalini@zhservices.com>
+* +------------------------------------------------------------------------------+
+*/
+
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
  */
-chdir(dirname(__DIR__));
+//SANITIZE ALL ESCAPES
+$sanitize_all_escapes=true;
+//
+//STOP FAKE REGISTER GLOBALS
+$fake_register_globals=false;
+//
+require_once(dirname(__FILE__)."/../../../globals.php");
+require_once(dirname(__FILE__)."/../../../../library/forms.inc");
+require_once(dirname(__FILE__)."/../../../../library/options.inc.php");
+require_once(dirname(__FILE__)."/../../../../library/acl.inc");
 
-// Decline static file requests back to the PHP built-in webserver
-if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
-    return false;
-}
+
+chdir(dirname(__DIR__));
 
 // Setup autoloading
 require 'init_autoloader.php';
 
 // Run the application!
 Zend\Mvc\Application::init(require 'config/application.config.php')->run();
+
---
+++ interface/modules/zend_modules/public/js/application/common.js
@@ -0,0 +1,50 @@
+ /* +-----------------------------------------------------------------------------+
+*    OpenEMR - Open Source Electronic Medical Record
+*    Copyright (C) 2013 Z&H Consultancy Services Private Limited <sam@zhservices.com>
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU Affero General Public License as
+*    published by the Free Software Foundation, either version 3 of the
+*    License, or (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU Affero General Public License for more details.
+*
+*    You should have received a copy of the GNU Affero General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*    @author  Remesh Babu S <remesh@zhservices.com>
+* +------------------------------------------------------------------------------+
+*/
+
+ /**
+  * Language Translator
+  *
+  * @param {string} msg
+  * @returns {undefined}   
+  */
+  function langTranslator(msg) {
+    var resultTranslated = '';
+    var path = window.location;
+    var arr = path.toString().split("public");
+    var count = arr[1].split("/").length-1;
+    var newpath = './';
+    for(var i = 0; i < count; i++){
+      newpath += '../'; 
+    }
+    $.ajax({
+      type: 'POST',
+      url: newpath + "public/application/index/langTranslator", 
+      async: false,
+      data:{
+				msg: msg
+				},
+      success: function(result){
+        resultTranslated = result;
+      }
+    });
+    return resultTranslated;
+  }
+
+
